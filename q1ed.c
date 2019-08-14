2859e133dd11c153450bb3510b1330f92f83a06d
BarrosLucas <lucasfreitasdebarros@gmail.com>
2019-08-14 11:40:19 -0300

    Add files via upload

---
+++ q1ed.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fila{
+    int *queue;
+    int size;
+    int first;
+    int last;
+} fila;
+
+int push(fila*damae, int value){
+    //printf("Value: %d\n",value);
+    (damae)->last++;
+    if(((damae)->last - (damae)->first)==((damae)->size)){
+        (damae)->size *= 2;
+        int * newArray=(int *) malloc(sizeof(int)*(damae)->size);
+        for(int i = damae->first;i < damae->last;i++){
+            newArray[i] = damae->queue[i];
+        }
+        damae->queue = newArray;
+
+
+    }
+    (damae)->queue[(damae)->last] = value;
+    //(damae)->queue[]
+    return 1;
+}
+
+int pop(fila*damae){
+    if((damae)->last < (damae)->first){
+        return -1;
+    }else{
+        int value = (damae)->queue[(damae)->first];
+        (damae)->first++;
+        return value;
+    }
+}
+
+void printList(fila*damae){
+    for(int i = (damae)->first;i<=(damae)->last;i++){
+        printf("%d ",(damae)->queue[i]);
+    }
+}
+
+int main (void){
+
+    fila daputa;
+
+
+    daputa.size=5;
+    daputa.first=0;
+    daputa.last=-1;
+    daputa.queue = (int *) malloc(sizeof(int)*daputa.size);
+
+    for(int i=0;i<50;i++){
+        push(&daputa,i);
+    }
+
+    printList(&daputa);
+}
